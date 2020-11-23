2ce3a77021349d366c6adb83cae2cd61a1a21b0e
vargapeter <omnis6@gmail.com>
2020-11-23 21:20:39 +0100

    Arrays and fuctions

--- week-2/day-1/Exercises/Arrays/src/Matrix.java
+++ week-2/day-1/Exercises/Arrays/src/Matrix.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
+
 public class Matrix {
     public static void main(String[] args) {
-
         // - Create (dynamically*) a two dimensional array
         //   with the following matrix**. Use a loop!
         //
@@ -12,6 +13,26 @@ public class Matrix {
         // - Print this two dimensional array to the output
         // * length should depend on a variable
         // ** Relax, a matrix is just like an array
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many colums?");
+        int size = scan.nextInt();
+
+        scan.close();
+
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = 1;
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
+
 }
