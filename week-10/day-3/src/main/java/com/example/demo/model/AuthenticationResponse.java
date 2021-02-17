fe7e5bd514d8a0cc7a05d693e752b6cf12add964
vargapeter <omnis6@gmail.com>
2021-02-17 14:20:54 +0100

    authentication spring web security

---
+++ week-10/day-3/src/main/java/com/example/demo/model/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+}
